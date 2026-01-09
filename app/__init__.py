0d0680f7ed887a5f3cf98325248a4dae818abb87
unknown <mayankprajapat615@gmail.com>
2026-01-09 12:36:52 +0530

    Added user icon and redesigned navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,25 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def create_app():
-    app =Flask(__name__)
+    app = Flask(__name__)
 
-    #this is for scretkey 
     app.config['SECRET_KEY'] = 'your-secret-key'
-
-    #these lines are dedicated to the database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    
-    #these are for the forms and blueprints & routes
+
+    @app.context_processor
+    def inject_user():
+        from app.models.user import User
+        user = None
+        if 'user_id' in session:
+            user = User.query.get(session['user_id'])
+        return dict(current_user=user)
+
     from app.routes.auth import auth_bp
     from app.routes.task import tasks_bp
     app.register_blueprint(auth_bp)
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,13 @@
 console.log("Script loaded successfully.");
+
+const panel = document.getElementById('profile-panel');
+
+function toggleProfile(event) {
+  event.stopPropagation(); // VERY IMPORTANT
+  panel.classList.toggle('open');
+}
+
+// close when clicking anywhere else
+document.addEventListener('click', function () {
+  panel.classList.remove('open');
+});
